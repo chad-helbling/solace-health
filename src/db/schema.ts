16770ddbff8a12a6d24bc85155489174953600af
chad-helbling <chad.helbling.1@gmail.com>
2025-03-09 14:08:59 -0500

    chore: updates schema for full text search

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -7,18 +7,28 @@ import {
   serial,
   timestamp,
   bigint,
+  index,
 } from "drizzle-orm/pg-core";
 
-const advocates = pgTable("advocates", {
-  id: serial("id").primaryKey(),
-  firstName: text("first_name").notNull(),
-  lastName: text("last_name").notNull(),
-  city: text("city").notNull(),
-  degree: text("degree").notNull(),
-  specialties: jsonb("payload").default([]).notNull(),
-  yearsOfExperience: integer("years_of_experience").notNull(),
-  phoneNumber: bigint("phone_number", { mode: "number" }).notNull(),
-  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
-});
+const advocates = pgTable(
+  "advocates",
+  {
+    id: serial("id").primaryKey(),
+    firstName: text("first_name").notNull(),
+    lastName: text("last_name").notNull(),
+    city: text("city").notNull(),
+    degree: text("degree").notNull(),
+    specialties: jsonb("payload").default([]).notNull(),
+    yearsOfExperience: integer("years_of_experience").notNull(),
+    phoneNumber: bigint("phone_number", { mode: "number" }).notNull(),
+    createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
+  },
+  (table) => [
+    index("advocates_search_index").using(
+      "gin",
+      sql`to_tsvector('english', ${table.firstName} || ' ' || ${table.lastName} || ' ' || ${table.city} || ' ' || ${table.degree} || ' ' || coalesce(${table.specialties}::text, ''))`,
+    ),
+  ],
+);
 
 export { advocates };
